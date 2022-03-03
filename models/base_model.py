7ea1c63443441ce0930267dc2ec30704172ab09f
MontassarBar <montassarbarbouchi7@gmail.com>
2022-03-03 15:02:20 +0100

    BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,20 @@
+import uuid
+from datetime import datetime
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+    def __str__(self):
+        return "[{}] (<{}>) <{}>".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        dictionary =  self.__dict__
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = datetime.now().isoformat(sep='T')
+        dictionary['updated_at'] = datetime.now().isoformat(sep='T')
+        return dictionary
