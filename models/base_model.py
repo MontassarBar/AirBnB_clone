9acab1b537ca72dd9c51cbff0b97a7af3f6fe02b
MontassarBar <montassarbarbouchi7@gmail.com>
2022-03-03 16:56:58 +0100

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -8,9 +8,6 @@ class BaseModel:
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key != '__class__':
                     setattr(self, key, value)
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.today()
