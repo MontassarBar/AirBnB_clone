7b15b9f208fe13f476941b372a495f55ffe9c544
MontassarBar <montassarbarbouchi7@gmail.com>
2022-03-03 16:52:24 +0100

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,19 @@
 import uuid
 from datetime import datetime
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
 
     def __str__(self):
         return "[{}] (<{}>) <{}>".format(self.__class__.__name__, self.id, self.__dict__)
@@ -13,7 +22,7 @@ class BaseModel:
         self.updated_at = datetime.today()
 
     def to_dict(self):
-        dictionary =  self.__dict__
+        dictionary =  self.__dict__.copy()
         dictionary['__class__'] = self.__class__.__name__
         dictionary['created_at'] = datetime.now().isoformat(sep='T')
         dictionary['updated_at'] = datetime.now().isoformat(sep='T')
