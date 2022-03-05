b9b946ad44170fc927fc68bdd9b0e2ddd19dd1a6
MontassarBar <montassarbarbouchi7@gmail.com>
2022-03-05 19:43:14 +0100

    FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,19 +11,20 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+        if obj:
+            self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
         dicty = {}
         for key, value in self.__objects.items():
             dicty[key] = value.to_dict()
-        with open(self.__file_path, mode="w", encoding="utf-8") as MyFile:
-            json.dump(dicty, MyFile)
+        with open(self.__file_path, mode="w", encoding="utf-8") as x:
+            json.dump(dicty, x)
 
     def reload(self):
         try:
-            with open(self.__file_path, mode="r", encoding="utf-8") as MyFile:
-                f = json.load(MyFile)
+            with open(self.__file_path, mode="r", encoding="utf-8") as x:
+                f = json.load(x)
             for key, value in f.items():
                 name = key.split('.')[0]
                 self.__objects[key] = eval(name)(**value)
