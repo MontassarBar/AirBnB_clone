1327a7ffa443087fcf9730ee2e1e2510df745448
MontassarBar <montassarbarbouchi7@gmail.com>
2022-03-06 17:50:43 +0100

    State

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''State class'''
+from models.base_model import BaseModel
+
+
+class State:
+    '''
+    Public class attributes:
+        name: string - empty string
+    '''
+    name = ""
