83a96f96f097e0775b136f02e54ff86664962607
MontassarBar <montassarbarbouchi7@gmail.com>
2022-03-06 20:07:02 +0100

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -25,6 +25,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.today()
+            self.updated_at = datetime.today()
             models.storage.new(self)
 
     def __str__(self):
