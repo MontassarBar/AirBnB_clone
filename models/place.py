6a951e9f364ce514e9b61627f17e50287a454095
MontassarBar <91077064+MontassarBar@users.noreply.github.com>
2022-03-06 18:07:38 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel
 
 
-class Place:
+class Place(BaseModel):
     '''
     Public class attributes:
         city_id: string - empty string: it will be the City.id
